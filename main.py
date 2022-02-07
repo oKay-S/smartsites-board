fd9cb5c84e1d6322ff2c34a8a53dad5e504188aa
KieranSoutter <72031734+KieranSoutter@users.noreply.github.com>
2022-02-07 21:34:03 +0000

    Packet Detection Stage 4 - Removed Routers from packet detection, shows low and inconsistent amount of devices.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import os
+import re
 import time
 
+import hashlib
 import pcapy
+import subprocess
 
-from clock import Clock  # imports the clock class
-from scan import Scan   # imports the scan class
+from scan import Scan  # imports the scan class
 import lcddriver
+import os
 
 scan = Scan()
 
@@ -14,37 +16,56 @@ lcd.lcd_clear()
 program = 0  # sets program to 0 for infinite loop creation
 display = lcddriver.lcd()
 packets = []
+router_list = []
 lastprinttime = time.time()
+
+mac_match = re.compile("[A-Za-z0-9]{2}:[A-Za-z0-9]{2}:[A-Za-z0-9]{2}:[A-Za-z0-9]{2}:[A-Za-z0-9]{2}:[A-Za-z0-9]{2}",
+                       flags=re.I)
 
 
 def addpackets(header, data):
-
     global lastprinttime
     global packets
+    global router_list
 
     packet = scan.packet_handler(header, data)
+    routers = subprocess.check_output("sudo iwlist wlan0 scan | grep 'Address'", shell=True)
+    routers_str = str(routers)
+    routers_str_upper = routers_str.upper()  # although currently the command returns in uppercase, it may not in future iterations of the command
+    routers_regexed = mac_match.findall(routers_str_upper)
+    for i in routers_regexed:
+        routers_hashed = hashlib.sha512(i.encode("utf-8")).hexdigest()
+        if routers_hashed not in router_list:
+            router_list.append(routers_hashed)
+
+
+
     if packet is not None:
-        if packet not in packets:
-            packets.append(packet)
+        for address in packet:
+            if address not in packets:
+                if address not in router_list:
+                    packets.append(address)
+    else:
+        print("packet")
 
     if (lastprinttime + 60) < time.time():
         lastprinttime = time.time()
-        os.system("clear")
+        lcd.lcd_clear()
         print("a minute passes!")
         length = len(packets)
-        print(str(length))
+        lengthstr = str(length)
+        lcd.lcd_display_string("Number of", 1)
+        lcd.lcd_display_string("devices: " + lengthstr, 2)
+        time.sleep(1)
+
         print(packets)
         packets = []
 
+
 def main():
-    print(Clock.time())
     lcd.lcd_clear()
-    lcd.lcd_display_string(Clock.time() + " " + Clock.date(), 1)
     p = pcapy.open_live("wlan1mon", 2000, 0, 1000)
     p.loop(-1, addpackets)
-
-
-
 
 
 if __name__ == '__main__':
--- scan.py
+++ scan.py
@@ -1,6 +1,8 @@
 import impacket
 import hashlib
+from scapy.all import *
 from impacket.ImpactDecoder import RadioTapDecoder
+from scapy.layers.dot11 import Dot11Beacon
 
 
 class Scan:
@@ -10,14 +12,156 @@ class Scan:
         radio_packet = self.DECODER.decode(data)
         dot11 = radio_packet.child()
         management_base = dot11.child()
+
+        addresses = []
+
         if management_base.__class__ == impacket.dot11.Dot11DataFrame:
             s_address = management_base.get_address1()
-            #   d_address = management_base.get_destination_address()
+            p_address = management_base.get_address2()
+            d_address = management_base.get_address3()
 
             parsed_s_address = "".join('{:02x}:'.format(x) for x in s_address)[:-1]
             parsed_s_address_stringed = parsed_s_address.encode("utf-8")
-            parsed_s_address_hashed = hashlib.sha512(parsed_s_address_stringed).hexdigest()
+            parsed_s_address_stringed_upper = parsed_s_address_stringed.upper()
+            parsed_s_address_hashed = hashlib.sha512(parsed_s_address_stringed_upper).hexdigest()
 
-            return parsed_s_address_hashed
+            parsed_p_address = "".join('{:02x}:'.format(x) for x in p_address)[:-1]
+            parsed_p_address_stringed = parsed_p_address.encode("utf-8")
+            parsed_p_address_stringed_upper = parsed_p_address_stringed.upper()
+            parsed_p_address_hashed = hashlib.sha512(parsed_p_address_stringed_upper).hexdigest()
 
+            parsed_d_address = "".join('{:02x}:'.format(x) for x in d_address)[:-1]
+            parsed_d_address_stringed = parsed_d_address.encode("utf-8")
+            parsed_d_address_stringed_upper = parsed_d_address_stringed.upper()
+            parsed_d_address_hashed = hashlib.sha512(parsed_d_address_stringed_upper).hexdigest()
 
+            addresses.append(parsed_s_address_hashed)
+            addresses.append(parsed_p_address_hashed)
+            addresses.append(parsed_d_address_hashed)
+
+            return addresses
+
+        elif management_base.__class__ == impacket.dot11.Dot11ManagementFrame:
+
+            s_address = management_base.get_source_address()
+            d_address = management_base.get_destination_address()
+
+            parsed_s_address = "".join('{:02x}:'.format(x) for x in s_address)[:-1]
+            parsed_s_address_stringed = parsed_s_address.encode("utf-8")
+            parsed_s_address_stringed_upper = parsed_s_address_stringed.upper()
+            parsed_s_address_hashed = hashlib.sha512(parsed_s_address_stringed_upper).hexdigest()
+
+            parsed_d_address = "".join('{:02x}:'.format(x) for x in d_address)[:-1]
+            parsed_d_address_stringed = parsed_d_address.encode("utf-8")
+            parsed_d_address_stringed_upper = parsed_d_address_stringed.upper()
+            parsed_d_address_hashed = hashlib.sha512(parsed_d_address_stringed_upper).hexdigest()
+
+            addresses.append(parsed_s_address_hashed)
+            addresses.append(parsed_d_address_hashed)
+
+            return addresses
+
+        elif management_base.__class__ == impacket.dot11.Dot11ControlFrameCFEndCFACK:
+            address = management_base.get_ra()
+
+            parsed_address = "".join('{:02x}:'.format(x) for x in address)[:-1]
+            parsed_address_stringed = parsed_address.encode("utf-8")
+            parsed_address_stringed_upper = parsed_address_stringed.upper()
+            parsed_address_hashed = hashlib.sha512(parsed_address_stringed_upper).hexdigest()
+
+            addresses.append(parsed_address_hashed)
+
+            return addresses
+
+        elif management_base.__class__ == impacket.dot11.Dot11ControlFrameRTS:
+            address = management_base.get_ra()
+
+            parsed_address = "".join('{:02x}:'.format(x) for x in address)[:-1]
+            parsed_address_stringed = parsed_address.encode("utf-8")
+            parsed_address_stringed_upper = parsed_address_stringed.upper()
+            parsed_address_hashed = hashlib.sha512(parsed_address_stringed_upper).hexdigest()
+
+            addresses.append(parsed_address_hashed)
+
+            return addresses
+
+        elif management_base.__class__ == impacket.dot11.Dot11ControlFrameCFEnd:
+            address = management_base.get_ra()
+
+            parsed_address = "".join('{:02x}:'.format(x) for x in address)[:-1]
+            parsed_address_stringed = parsed_address.encode("utf-8")
+            parsed_address_stringed_upper = parsed_address_stringed.upper()
+            parsed_address_hashed = hashlib.sha512(parsed_address_stringed_upper).hexdigest()
+
+            addresses.append(parsed_address_hashed)
+
+            return addresses
+
+        elif management_base.__class__ == impacket.dot11.Dot11ControlFramePSPoll:
+            address = management_base.get_ta()
+
+            parsed_address = "".join('{:02x}:'.format(x) for x in address)[:-1]
+            parsed_address_stringed = parsed_address.encode("utf-8")
+            parsed_address_stringed_upper = parsed_address_stringed.upper()
+            parsed_address_hashed = hashlib.sha512(parsed_address_stringed_upper).hexdigest()
+
+            addresses.append(parsed_address_hashed)
+
+            return addresses
+
+        elif management_base.__class__ == impacket.dot11.Dot11ControlFrameRTS:
+            s_address = management_base.get_ta()
+            d_address = management_base.get_ra()
+
+            parsed_s_address = "".join('{:02x}:'.format(x) for x in s_address)[:-1]
+            parsed_s_address_stringed = parsed_s_address.encode("utf-8")
+            parsed_s_address_stringed_upper = parsed_s_address_stringed.upper()
+            parsed_s_address_hashed = hashlib.sha512(parsed_s_address_stringed_upper).hexdigest()
+
+            parsed_d_address = "".join('{:02x}:'.format(x) for x in d_address)[:-1]
+            parsed_d_address_stringed = parsed_d_address.encode("utf-8")
+            parsed_d_address_stringed_upper = parsed_d_address_stringed.upper()
+            parsed_d_address_hashed = hashlib.sha512(parsed_d_address_stringed_upper).hexdigest()
+
+            addresses.append(parsed_s_address_hashed)
+            addresses.append(parsed_d_address_hashed)
+
+            return addresses
+
+        elif management_base.__class__ == impacket.dot11.Dot11ControlFrameACK:
+                s_address = management_base.get_ta()
+                d_address = management_base.get_ra()
+
+                parsed_s_address = "".join('{:02x}:'.format(x) for x in s_address)[:-1]
+                parsed_s_address_stringed = parsed_s_address.encode("utf-8")
+                parsed_s_address_stringed_upper = parsed_s_address_stringed.upper()
+                parsed_s_address_hashed = hashlib.sha512(parsed_s_address_stringed_upper).hexdigest()
+
+                parsed_d_address = "".join('{:02x}:'.format(x) for x in d_address)[:-1]
+                parsed_d_address_stringed = parsed_d_address.encode("utf-8")
+                parsed_d_address_stringed_upper = parsed_d_address_stringed.upper()
+                parsed_d_address_hashed = hashlib.sha512(parsed_d_address_stringed_upper).hexdigest()
+
+                addresses.append(parsed_s_address_hashed)
+                addresses.append(parsed_d_address_hashed)
+
+                return addresses
+
+        elif management_base.__class__ == impacket.dot11.Dot11ControlFrameCTS:
+                s_address = management_base.get_ta()
+                d_address = management_base.get_ra()
+
+                parsed_s_address = "".join('{:02x}:'.format(x) for x in s_address)[:-1]
+                parsed_s_address_stringed = parsed_s_address.encode("utf-8")
+                parsed_s_address_stringed_upper = parsed_s_address_stringed.upper()
+                parsed_s_address_hashed = hashlib.sha512(parsed_s_address_stringed_upper).hexdigest()
+
+                parsed_d_address = "".join('{:02x}:'.format(x) for x in d_address)[:-1]
+                parsed_d_address_stringed = parsed_d_address.encode("utf-8")
+                parsed_d_address_stringed_upper = parsed_d_address_stringed.upper()
+                parsed_d_address_hashed = hashlib.sha512(parsed_d_address_stringed_upper).hexdigest()
+
+                addresses.append(parsed_s_address_hashed)
+                addresses.append(parsed_d_address_hashed)
+
+                return addresses
