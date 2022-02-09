f709d04efe1e2a2db05521f3c395a84d26fbfdca
KieranSoutter <72031734+KieranSoutter@users.noreply.github.com>
2022-02-09 17:03:39 +0000

    Packet Detection Stage 4.3 - Increased detection timings to increase consistency and added try catch statements for ease of use (also added status messages to connected lcd boards)

--- main.py
+++ main.py
@@ -28,31 +28,45 @@ def addpackets(header, data):
     global router_list
 
     packet = scan.packet_handler(header, data)
-    routers = subprocess.check_output("sudo iwlist wlan0 scan | grep 'Address'", shell=True)
-    routers_str = str(routers)
-    routers_str_upper = routers_str.upper()  # although currently the command returns in uppercase, it may not in future iterations of the command
-    routers_regexed = mac_match.findall(routers_str_upper)
-    for i in routers_regexed:
-        routers_hashed = hashlib.sha512(i.encode("utf-8")).hexdigest()
-        if routers_hashed not in router_list:
-            router_list.append(routers_hashed)
+
+    try:
+        routers = subprocess.check_output("sudo iwlist wlan0 scan | grep 'Address'", shell=True)
+        routers_str = str(routers)
+        routers_str_upper = routers_str.upper()  # although currently the command returns in uppercase, it may not in future iterations of the command
+        routers_regexed = mac_match.findall(routers_str_upper)
+        for i in routers_regexed:
+            routers_hashed = hashlib.sha512(i.encode("utf-8")).hexdigest()
+            if routers_hashed not in router_list:
+                router_list.append(routers_hashed)
+    except:
+        print("scan failed")
 
     if packet is not None:
         for address in packet:
             if address not in packets:
                 if address not in router_list:
                     packets.append(address)
-    else:
-        print("improper packet")
+                    lcd.lcd_display_string("                     ",
+                                           1)  # Horrible way of doing this however there is no driver support for clearing just one line
+                    lcd.lcd_display_string("Device Found", 1)
+                else:
+                    lcd.lcd_display_string("                     ",
+                                           1)  # see previous comment
+                    lcd.lcd_display_string("Router Found", 1)
 
-    if (lastprinttime + 60) < time.time():
+    else:
+        lcd.lcd_display_string("                     ",
+                               1)  # see previous comment
+        lcd.lcd_display_string("Improper Packet", 1)
+
+    if (lastprinttime + 300) < time.time():
         lastprinttime = time.time()
         lcd.lcd_clear()
         print("a minute passes!")
         length = len(packets)
         lengthstr = str(length)
         lcd.lcd_display_string("Number of", 1)
-        lcd.lcd_display_string("devices: " + lengthstr, 2)
+        lcd.lcd_display_string("Devices: " + lengthstr, 2)
         time.sleep(1)
 
         print(packets)
@@ -61,8 +75,28 @@ def addpackets(header, data):
 
 def main():
     lcd.lcd_clear()
-    p = pcapy.open_live("wlan1mon", 2000, 0, 1000)
-    p.loop(-1, addpackets)
+    lcd.lcd_display_string("System", 1)
+    lcd.lcd_display_string("ok ", 2)
+    time.sleep(1)
+    try:
+        p = pcapy.open_live("wlan1mon", 2000, 0, 1000)
+        lcd.lcd_clear()
+        lcd.lcd_display_string("Scanner", 1)
+        lcd.lcd_display_string("ok", 2)
+        time.sleep(1)
+        try:
+            lcd.lcd_clear()
+            lcd.lcd_display_string("Scanning", 1)
+            lcd.lcd_display_string("Active", 2)
+            p.loop(-1, addpackets)
+        except:
+            lcd.lcd_clear()
+            lcd.lcd_display_string("Scanning", 1)
+            lcd.lcd_display_string("failed", 2)
+    except:
+        lcd.lcd_clear()
+        lcd.lcd_display_string("Scanner", 1)
+        lcd.lcd_display_string("Failed ", 2)
 
 
 if __name__ == '__main__':
